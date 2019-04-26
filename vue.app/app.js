7c1c3c2587e3273ecc423a6f96cdd2aa8586f68d
Raul Guerrero Carrasco <r.g.c@me.com>
2019-04-26 13:26:12 +0200

    Finished NoteMaster

--- vue.app/app.js
+++ vue.app/app.js
@@ -5,6 +5,22 @@ var app = new Vue({
         note: {
             title: '',
             text: ''
+        },
+        notes: JSON.parse(localStorage.getItem('notes'))
+    },
+    methods: {
+        addNote() {
+            let { text, title } = this.note
+            this.notes.push({
+                text,
+                title,
+                date: new Date(Date.now()).toLocaleString()
+            })
+            localStorage.setItem('notes', JSON.stringify(this.notes))
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1)
+            localStorage.setItem('notes', JSON.stringify(this.notes))
         }
     }
 });
